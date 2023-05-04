7b436a0377f8485eca513bf3b6f44617b36eae5b
Suraj Konangi <suraj.konangi@ATMECS.CORP>
2023-05-05 00:48:41 +0530

    automationupdate

--- src/test/java/com/apitraining/Automation/testscripts/ParentClass.java
+++ src/test/java/com/apitraining/Automation/testscripts/ParentClass.java
@@ -1,7 +1,5 @@
 package com.apitraining.Automation.testscripts;
 
-import org.testng.annotations.Test;
-
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
@@ -22,65 +20,66 @@ import org.testng.annotations.AfterSuite;
 
 public class ParentClass {
 	private Response response;
+	private Properties prop;
+	private JSONObject input;
+
 	
-  @Test
-  public void f() {
-  }
-  @BeforeMethod
-  public void beforeMethod() {
-  }
+	@BeforeMethod
+	public void beforeMethod() {
 
-  @AfterMethod
-  public void afterMethod() {
-  }
+	}
 
-  @BeforeClass
-  public void beforeClass() throws Exception {
-	  Properties prop = new Properties();
-	  FileInputStream path = new FileInputStream("C:\\Users\\suraj.konangi\\eclipse-workspace\\Automation\\src\\main\\resources\\Testdata.properties");
-	  prop.load(path);
-	  JSONObject input = new JSONObject();
-		input.put("name", prop.getProperty("name"));
-		input.put("salary", prop.getProperty("salary"));
-		input.put("age",prop.getProperty("age"));
-	   response = RestAssured.given().contentType(prop.getProperty("content")).body(input.toString()).auth().basic(prop.getProperty("user"),prop.getProperty("password")).when().post("/create").then().log()
-				.all().extract().response();
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("The status code is " + response.getStatusCode());
+		Assert.assertEquals(response.getStatusCode(), 200);
+	}
+
+	@BeforeClass
+	public void beforeClass() throws Exception {
+		
+		response = RestAssured.given().contentType(prop.getProperty("content")).body(input.toString()).auth()
+				.basic(prop.getProperty("user"), prop.getProperty("password")).when().post("/create").then().log().all()
+				.extract().response();
 		System.out.println(response);
-  }
+	}
 
-  @AfterClass
-  public void afterClass() {
-  }
+	@AfterClass
+	public void afterClass() {
+		System.out.println("the status time is " + response.getTime());
 
-  @BeforeTest
-  public void beforeTest() throws Exception {
-	  Properties prop = new Properties();
-//	  FileInputStream path = new FileInputStream("C:\\Users\\suraj.konangi\\eclipse-workspace\\Automation\\src\\main\\resources\\Testdata.properties");
-//	  prop.load(path);
-	  JSONObject input = new JSONObject();
+	}
+
+	@BeforeTest
+	public void beforeTest() throws Exception {
+ 
+		input = new JSONObject();
 		input.put("name", prop.getProperty("name"));
 		input.put("salary", prop.getProperty("salary"));
-		input.put("age",prop.getProperty("age"));
+		input.put("age", prop.getProperty("age"));
 		RestAssured.given().body(input.toString());
-  }
+	}
 
-  @AfterTest
-  public void afterTest() {
-	  
-	  Assert.assertEquals(response.getStatusLine(), "HTTP/1.1 200 OK" , "Validated Success");
-  }
+	@AfterTest
+	public void afterTest() {
+System.out.println("status line is" + response.getStatusLine());
+		Assert.assertEquals(response.getStatusLine(), "HTTP/1.1 200 OK", "Validated Success");
+	}
 
-  @BeforeSuite
-  public void beforeSuite() throws Exception {
-	  Properties prop = new Properties();
-	  FileInputStream path = new FileInputStream("C:\\Users\\suraj.konangi\\eclipse-workspace\\Automation\\src\\main\\resources\\Testdata.properties");
-	  prop.load(path);
-	  RestAssured.baseURI = prop.getProperty("url");
-	  
-  }
+	@BeforeSuite
+	public void beforeSuite() throws Exception {
+		prop = new Properties();
+		FileInputStream path = new FileInputStream(
+				"C:\\Users\\suraj.konangi\\eclipse-workspace\\Automation\\src\\main\\resources\\Testdata.properties");
+		prop.load(path);
+		RestAssured.baseURI = prop.getProperty("url");
 
-  @AfterSuite
-  public void afterSuite() {
-  }
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("The body is " + response.getBody().asString());
+		
+	}
 
 }
--- src/test/java/com/apitraining/Automation/testscripts/RunnerClass.java
+++ src/test/java/com/apitraining/Automation/testscripts/RunnerClass.java
@@ -2,15 +2,9 @@ package com.apitraining.Automation.testscripts;
 
 import org.testng.annotations.Test;
 
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-
 public class RunnerClass extends ParentClass{
   @Test
   public void post() {
 	
-		Response response = RestAssured.given().when().post("/create")
-				.then().log().all().extract().response();
-		System.out.println(response);
   }
 }
