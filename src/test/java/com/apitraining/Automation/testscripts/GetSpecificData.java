18169848deb5e02bfecf9be4e29ad1348d664347
Suraj Konangi <suraj.konangi@ATMECS.CORP>
2023-05-11 11:38:21 +0530

    updated code

--- src/test/java/com/apitraining/Automation/testscripts/GetSpecificData.java
+++ src/test/java/com/apitraining/Automation/testscripts/GetSpecificData.java
@@ -1,6 +1,9 @@
 package com.apitraining.Automation.testscripts;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -12,25 +15,35 @@ import io.restassured.response.Response;
 
 public class GetSpecificData {
 
+	private Pattern pattern;
+
 	@Test
 	public void get() {
+		pattern = Pattern.compile("^([0-9]+)");
 		String url = "https://dummy.restapiexample.com/api/v1/employees";
-
-		Response response = RestAssured.get(url);
+		Response response = RestAssured.given().get(url);
 		System.out.println(response.getBody().asString());
 
 		JsonPath jpath = response.jsonPath();
 		assertThat(response.getStatusCode(), is(200));
 		ArrayList<String> list = jpath.get("data.id");
 		System.out.println(list.size());
-		String employeename = "Tiger Nixon";
+		Matcher match = pattern.matcher("170750");
+		if (match.matches()) {
+			System.out.println(match.group(0));
+		}
 		for (int i = 0; i < list.size(); i++) {
-			if ((jpath.get("data.employee_name[" + i + "]")).equals(employeename)) {
+
+			if ((jpath.get("data.employee_salary[" + i + "]")).toString().equals(match.group(0))) {
+				System.out.println(match.group(0));
 				System.out.println("the employee name==>" + jpath.get("data.employee_name[" + i + "]"));
-				System.out.println("the employee age==>" + jpath.get("data.employee_age[" + i + "]"));
 				System.out.println("the employee salary==>" + jpath.get("data.employee_salary[" + i + "]"));
+				System.out.println("the employee age==>" + jpath.get("data.employee_age[" + i + "]"));
 
 			}
+
 		}
+
 	}
+
 }
